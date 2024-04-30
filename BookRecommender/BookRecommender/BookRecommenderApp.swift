814540b14054d15be9d5be38a281ab6efbf92b67
RazielApple <fbarbato22@fed.idserve.net>
2024-04-30 19:42:04 +0200

    First commit

---
+++ BookRecommender/BookRecommender/BookRecommenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookRecommenderApp.swift
+//  BookRecommender
+//
+//  Created by Fabio Barbato on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BookRecommenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
