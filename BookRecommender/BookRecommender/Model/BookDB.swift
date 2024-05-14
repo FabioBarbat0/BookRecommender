769dea30bb34b6c26ef18450932dd2e75c8040eb
RazielApple <fbarbato22@fed.idserve.net>
2024-05-14 20:32:30 +0200

    Update BookDB.swift

--- BookRecommender/BookRecommender/Model/BookDB.swift
+++ BookRecommender/BookRecommender/Model/BookDB.swift
@@ -10,10 +10,9 @@ import Foundation
 let bookDB = [
     Book(cover: "https://www.ibs.it/images/9788868368593_0_536_0_75.jpg", ISBN: "9788858513477", name: "La ragazza del treno"),
     Book(cover: "https://m.media-amazon.com/images/I/913oKWaXUCL._AC_UF1000,1000_QL80_.jpg", ISBN: "9788865830444", name: "La terapia"),
-    Book(cover: "https://www.ibs.it/images/9788868368593_0_536_0_75.jpg", ISBN: "9788858513477", name: "La ragazza del treno"),
-    Book(cover: "https://m.media-amazon.com/images/I/913oKWaXUCL._AC_UF1000,1000_QL80_.jpg", ISBN: "9788865830444", name: "La terapia"),
-    Book(cover: "https://www.ibs.it/images/9788868368593_0_536_0_75.jpg", ISBN: "9788858513477", name: "La ragazza del treno"),
-    Book(cover: "https://m.media-amazon.com/images/I/913oKWaXUCL._AC_UF1000,1000_QL80_.jpg", ISBN: "9788865830444", name: "La terapia"),
-    Book(cover: "https://www.ibs.it/images/9788868368593_0_536_0_75.jpg", ISBN: "9788858513477", name: "La ragazza del treno"),
-    Book(cover: "https://m.media-amazon.com/images/I/913oKWaXUCL._AC_UF1000,1000_QL80_.jpg", ISBN: "9788865830444", name: "La terapia")
+    Book(cover: "https://www.ibs.it/images/9788804738237_0_0_536_0_75.jpg", ISBN: "9788835707431", name: "La città di vapore"),
+    Book(cover: "https://www.ibs.it/images/9788852087226_0_536_0_75.jpg", ISBN: "9788852087226", name: "La donna alla finestra"),
+    Book(cover: "https://www.ibs.it/images/9788804667735_0_536_0_75.jpg", ISBN: "9788852077128", name: "La coppia della porta accanto"),
+    Book(cover: "https://www.ibs.it/images/9788850246731_0_536_0_75.jpg", ISBN: "9788850250998", name: "Altered Carbon"),
+    Book(cover: "https://www.ibs.it/images/9788834739679_0_536_0_75.jpg", ISBN: "9788834721285", name: "Dune")
 ]
